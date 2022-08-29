e378c3ca598935bde60c6a843fa5d1081713b051
joreims <jorge.garcia14624@est.uca.edu.ni>
2022-08-29 09:59:12 -0600

    Ejercicios en clase

---
+++ Arreglo1.py
@@ -0,0 +1,11 @@
+numeros = []
+numeros.append(3)
+numeros.append(8)
+numeros.append(9)
+numeros.append(20)
+print("Elementos del arreglo")
+print(numeros)
+numeros.insert(1, 15)
+print(numeros)
+numeros.remove(3)
+print(numeros)
